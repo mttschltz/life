7589d9be3c8382c162c4c467c1e540fd02cfb499
Matt Schultz <matt@mattschultz.info>
2021-06-16 09:04:55 +0930

    Test commit with type error to test CI

--- app/matt-fyi/src/pages/index.tsx
+++ app/matt-fyi/src/pages/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { createPlan } from "life/src/usecase/createPlan";
 
 const IndexPage = () => {
-  const plan = createPlan("plan name");
+  const plan = createPlan(1);
 
   return <main>Plan: {plan.name}</main>;
 };
