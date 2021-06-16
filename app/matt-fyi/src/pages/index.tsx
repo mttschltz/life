aa9d4856954be93102ac6ee19e8592d9b2fa868f
Matt Schultz <matt@mattschultz.info>
2021-06-16 20:23:10 +0930

    Add linting precommit hook with husky, lint-staged

--- app/matt-fyi/src/pages/index.tsx
+++ app/matt-fyi/src/pages/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { createPlan } from "life/src/usecase/createPlan";
 
 const IndexPage = () => {
-  const plan = createPlan(1);
+  const plan = createPlan("a plan");
 
   return <main>Plan: {plan.name}</main>;
 };
