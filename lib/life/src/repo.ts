b152bbae5799ba022d2c4f54850d7413c4090c3e
Matt Schultz <matt@mattschultz.info>
2021-08-20 22:01:36 +0930

    Fix function definition style linting error

--- lib/life/src/repo.ts
+++ lib/life/src/repo.ts
@@ -2,9 +2,9 @@ import { Result } from 'util/result'
 import { Category, Risk } from 'life/src/risk'
 
 export interface RiskRepo {
-  createRisk(risk: Risk): Promise<Result<void>>
-  fetchRisk(id: string): Promise<Result<Risk>>
-  fetchRiskChildren(id: string): Promise<Result<Risk[]>>
-  fetchRiskParent(id: string): Promise<Result<Risk | undefined>>
-  listRisks(category: Category | undefined, includeDescendents: boolean): Promise<Result<Risk[]>>
+  createRisk: (risk: Risk) => Promise<Result<void>>
+  fetchRisk: (id: string) => Promise<Result<Risk>>
+  fetchRiskChildren: (id: string) => Promise<Result<Risk[]>>
+  fetchRiskParent: (id: string) => Promise<Result<Risk | undefined>>
+  listRisks: (category: Category | undefined, includeDescendents: boolean) => Promise<Result<Risk[]>>
 }
