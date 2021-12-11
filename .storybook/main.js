1c9e8c0fa82695090659192a8085b24a30c5d5de
Matt Schultz <matt@mattschultz.info>
2021-12-11 11:58:51 +1030

    Add Storybook with dummy button component

---
+++ .storybook/main.js
@@ -0,0 +1,5 @@
+module.exports = {
+  stories: ['../lib/component/**/*.stories.mdx', '../lib/component/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: '@storybook/react',
+}
