6fbd4fe3d3a4db71e57d4a02f0a0c74b77887e24
Matt Schultz <matt@mattschultz.info>
2022-01-17 20:04:50 +1030

    Require 100% test coverage for jest to pass

--- jest.config.ts
+++ jest.config.ts
@@ -46,7 +46,14 @@ export default {
   // ],
 
   // An object that configures minimum threshold enforcement for coverage results
-  // coverageThreshold: undefined,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 
   // A path to a custom dependency extractor
   // dependencyExtractor: undefined,
