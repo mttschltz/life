1efb34513886cae1edc275b432fd008266d17b5a
Matt Schultz <matt@mattschultz.info>
2022-01-19 08:13:36 +1030

    Add Heading component to lib

---
+++ lib/component/type/Heading.stories.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { Heading } from './Heading'
+import styled from 'styled-components'
+import { Box } from '@component'
+
+const Divider = styled.div`
+  max-width: 30rem;
+  padding-bottom: 3rem;
+`
+
+const Simple: ComponentStory<typeof Heading> = () => {
+  return (
+    <>
+      <Divider>
+        <Heading level={1}>Heading level 1</Heading>
+        <Heading level={2}>Heading level 2</Heading>
+        <Heading level={3}>Heading level 3</Heading>
+        <Heading level={4}>Heading level 4</Heading>
+      </Divider>
+      <Divider>
+        <Box background="brand">
+          <Heading level={4}>Heading default margin</Heading>
+        </Box>
+        <Box background="border">
+          <Heading level={4} margin="small">
+            Margin small
+          </Heading>
+        </Box>
+        <Box background="brand">
+          <Heading level={4} margin={{ top: 'xsmall', bottom: 'none' }}>
+            Margin top xsmall, bottom none
+          </Heading>
+        </Box>
+      </Divider>
+    </>
+  )
+}
+
+export { Simple }
+
+// eslint-disable-next-line import/no-default-export
+export default {
+  title: 'Type/Heading',
+} as ComponentMeta<typeof Heading>
---
+++ lib/component/type/Heading.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Heading as HeadingGrommet } from 'grommet'
+import { BoxProps } from '@component/layout/Box'
+
+interface HeadingProps {
+  level: 1 | 2 | 3 | 4
+  margin?: BoxProps['margin']
+}
+
+const Heading: React.FC<HeadingProps> = (props) => {
+  return <HeadingGrommet {...props} />
+}
+
+export { Heading }
