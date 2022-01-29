3f1774b8114cba8d19d17acb6fdcf341a4bdaf30
Matt Schultz <matt@mattschultz.info>
2022-01-29 18:46:03 +1030

    Add navigation tabs to layout

--- app/matt-fyi/src/component/layout/Layout.tsx
+++ app/matt-fyi/src/component/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { TranslationProvider, useTranslate } from '@matt-fyi/util/i18n/translate'
-import { Box, ThemeProvider, THEME, Header, Heading, Text, Main } from '@component'
+import { Box, ThemeProvider, THEME, Header, Heading, Text, Main, Tabs, Tab } from '@component'
 
 const Layout: React.FC = (props) => {
   const t = useTranslate('page')
@@ -10,10 +10,18 @@ const Layout: React.FC = (props) => {
       <TranslationProvider>
         <Box width="100%" direction="column" align="center">
           <Box width={{ width: '100%', max: 'large' }} direction="column">
-            <Header background="brand" pad="small" align="baseline">
-              <Heading level={1}>{t('page:home.title')}</Heading>
+            <Header background="brand" pad="small" align="baseline" direction="row-responsive">
+              <Heading level={1} size={'small'}>
+                {t('page:home.title')}
+              </Heading>
               <Text>{t('page:home.subtitle')}</Text>
             </Header>
+            <Box as="nav" align="start" margin={{ vertical: 'medium' }}>
+              <Tabs>
+                <Tab title={t('page:home.nav_plan')} icon="plan"></Tab>
+                <Tab title={t('page:home.nav_action')} icon="checkbox-selected"></Tab>
+              </Tabs>
+            </Box>
             <Main direction="column">{props.children}</Main>
           </Box>
         </Box>
--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -1,10 +1,25 @@
 describe('Home', () => {
-  describe('Updates', () => {
+  describe('Page', () => {
     it('renders', () => {
       cy.visit('')
+
+      // Header
       cy.contains('systemized.blog').closest('header')
       cy.contains('How to thrive, long-term').closest('header')
       cy.get('main').should('have.length', 1)
+
+      // Tabs
+      cy.get('nav').should('contain.text', 'Plan')
+      cy.get('nav')
+        .should('have.length', 1)
+        .find('button')
+        .should(($buttons) => {
+          expect($buttons).to.have.length(2)
+          expect($buttons.eq(0)).to.contain('Plan')
+          expect($buttons.eq(1)).to.contain('Action')
+        })
+
+      // Updates
       cy.contains('Updates')
     })
   })
