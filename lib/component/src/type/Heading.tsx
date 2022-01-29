7d09c5d618100ed695d912fd097ff20e2f1fbb77
Matt Schultz <matt@mattschultz.info>
2022-01-29 18:24:22 +1030

    Add size prop to Heading component

--- lib/component/src/type/Heading.tsx
+++ lib/component/src/type/Heading.tsx
@@ -5,6 +5,8 @@ import { BoxProps } from '@component/layout/Box'
 interface HeadingProps {
   level: 1 | 2 | 3 | 4
   margin?: BoxProps['margin']
+  // eslint-disable-next-line @typescript-eslint/sort-type-union-intersection-members
+  size?: 'small' | 'medium' | 'large' | 'xlarge'
 }
 
 const Heading: React.FC<HeadingProps> = (props) => {
