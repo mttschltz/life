d68bc263b52339f6ac88fe2162b89438ad59f4ac
Matt Schultz <matt@mattschultz.info>
2022-02-02 08:23:10 +1030

    Add fill property to Stack component

--- lib/component/src/layout/Stack.stories.tsx
+++ lib/component/src/layout/Stack.stories.tsx
@@ -17,30 +17,102 @@ const Simple: ComponentStory<typeof Stack> = () => {
       <Divider>
         <Text>Default anchor</Text>
         <Stack>
-          <Box background="brand" pad="large" />
-          <Box background="border" pad="xsmall" />
+          <Box background="brand" width="100%" height="small" />
+          <Box background="border" width="xxsmall" height="xxsmall" />
         </Stack>
       </Divider>
       <Divider>
         <Text>Center anchor</Text>
         <Stack anchor="center">
-          <Box background="brand" pad="large" />
-          <Box background="border" pad="xsmall" />
+          <Box background="brand" width="100%" height="small" />
+          <Box background="border" width="xxsmall" height="xxsmall" />
         </Stack>
       </Divider>
       <Divider>
         <Text>Left anchor</Text>
         <Stack anchor="left">
-          <Box background="brand" pad="large" />
-          <Box background="border" pad="xsmall" />
+          <Box background="brand" width="100%" height="small" />
+          <Box background="border" width="xxsmall" height="xxsmall" />
         </Stack>
       </Divider>
       <Divider>
         <Text>Top-right anchor</Text>
         <Stack anchor="top-right">
-          <Box background="brand" pad="large" />
-          <Box background="border" pad="xsmall" />
+          <Box background="brand" width="100%" height="small" />
+          <Box background="border" width="xxsmall" height="xxsmall" />
         </Stack>
+      </Divider>
+      <Divider>
+        <Text>No fill (parent direction: row)</Text>
+        <Box width="small" height="small" background="brand" justify="start" align="start">
+          <Stack anchor="bottom-right">
+            <Box width="xxsmall" height="xxsmall" background="border" />
+            <Box width="xxsmall" height="xxsmall" background="graph-1" />
+          </Stack>
+        </Box>
+      </Divider>
+      <Divider>
+        <Text>No fill (parent direction: column)</Text>
+        <Box width="small" height="small" background="brand" direction="column" justify="start" align="start">
+          <Stack anchor="bottom-right">
+            <Box width="xxsmall" height="xxsmall" background="border" />
+            <Box width="xxsmall" height="xxsmall" background="graph-1" />
+          </Stack>
+        </Box>
+      </Divider>
+      <Divider>
+        <Text>Horizontal fill (parent direction: row)</Text>
+        <Box width="small" height="small" background="brand" justify="start" align="start">
+          <Stack fill="horizontal" anchor="bottom-right">
+            <Box width="xxsmall" height="xxsmall" background="border" />
+            <Box width="xxsmall" height="xxsmall" background="graph-1" />
+          </Stack>
+        </Box>
+      </Divider>
+      <Divider>
+        <Text>Horizontal fill (parent direction: column)</Text>
+        <Box width="small" height="small" background="brand" direction="column" justify="start" align="start">
+          <Stack fill="horizontal" anchor="bottom-right">
+            <Box width="xxsmall" height="xxsmall" background="border" />
+            <Box width="xxsmall" height="xxsmall" background="graph-1" />
+          </Stack>
+        </Box>
+      </Divider>
+      <Divider>
+        <Text>Vertical fill (parent direction: row)</Text>
+        <Box width="small" height="small" background="brand" justify="start" align="start">
+          <Stack fill="vertical" anchor="bottom-right">
+            <Box width="xxsmall" height="xxsmall" background="border" />
+            <Box width="xxsmall" height="xxsmall" background="graph-1" />
+          </Stack>
+        </Box>
+      </Divider>
+      <Divider>
+        <Text>Vertical fill (parent direction: column)</Text>
+        <Box width="small" height="small" background="brand" direction="column" justify="start" align="start">
+          <Stack fill="vertical" anchor="bottom-right">
+            <Box width="xxsmall" height="xxsmall" background="border" />
+            <Box width="xxsmall" height="xxsmall" background="graph-1" />
+          </Stack>
+        </Box>
+      </Divider>
+      <Divider>
+        <Text>Both fill (parent direction: row)</Text>
+        <Box width="small" height="small" background="brand" justify="start" align="start">
+          <Stack fill="both" anchor="bottom-right">
+            <Box width="xxsmall" height="xxsmall" background="border" />
+            <Box width="xxsmall" height="xxsmall" background="graph-1" />
+          </Stack>
+        </Box>
+      </Divider>
+      <Divider>
+        <Text>Both fill (parent direction: column)</Text>
+        <Box width="small" height="small" background="brand" direction="column" justify="start" align="start">
+          <Stack fill="both" anchor="bottom-right">
+            <Box width="xxsmall" height="xxsmall" background="border" />
+            <Box width="xxsmall" height="xxsmall" background="graph-1" />
+          </Stack>
+        </Box>
       </Divider>
     </>
   )
--- lib/component/src/layout/Stack.tsx
+++ lib/component/src/layout/Stack.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { Stack as GrommetStack, StackProps as GrommetStackProps } from 'grommet'
+import { BoxProps } from './Box'
 
 interface StackProps {
   anchor?: GrommetStackProps['anchor']
+  fill?: BoxProps['fill']
 }
 
 const Stack: React.FC<StackProps> = (props) => {
-  return <GrommetStack {...props}>{props.children}</GrommetStack>
+  const { children, fill, ...forwardedProps } = props
+  return (
+    <GrommetStack fill={fill === 'both' ? true : fill} {...forwardedProps}>
+      {children}
+    </GrommetStack>
+  )
 }
 
 export { Stack }
