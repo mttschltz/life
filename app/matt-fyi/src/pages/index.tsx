f3635d263d3ffff4525fc580727b27392b6c0060
Matt Schultz <matt@mattschultz.info>
2022-02-05 18:15:46 +1030

    Customize index diagram for small viewports
    Also changes Cypress tests to run on two viewports.

--- app/matt-fyi/src/pages/index.tsx
+++ app/matt-fyi/src/pages/index.tsx
@@ -1,10 +1,11 @@
-import { Box, Button, Diagram, GatsbyLink, Heading, Stack, Text } from '@component'
+import { Box, Button, Diagram, GatsbyLink, Heading, ResponsiveConsumer, Stack, Text } from '@component'
 import { BoxProps } from '@component/layout/Box'
 import { filterNonNullish } from '@matt-fyi/util/graph/graph'
 import { useTranslate } from '@matt-fyi/util/i18n/translate'
 import { useRoute } from '@matt-fyi/util/route/route'
 import { graphql, PageProps } from 'gatsby'
 import * as React from 'react'
+import { ReactNode } from 'react'
 
 /* eslint-disable @typescript-eslint/naming-convention */
 const CATEGORY_NAME_COLOR_MAP: Record<string, BoxProps['background']> = {
@@ -14,7 +15,7 @@ const CATEGORY_NAME_COLOR_MAP: Record<string, BoxProps['background']> = {
 }
 /* eslint-enable @typescript-eslint/naming-convention */
 
-const categoryChildrenLayouts = (
+const verticalCategoryChildrenLayouts = (
   children: Pick<GatsbyTypes.Store_Category, 'id' | 'name'>[],
 ): { basis: BoxProps['basis']; justify: BoxProps['justify'] }[] => {
   return children.map((_, i) => {
@@ -56,59 +57,111 @@ const IndexPage: React.FunctionComponent<PageProps<GatsbyTypes.CategoryQueryQuer
       <Box direction="column" as="nav" testId="index--category-nav">
         {props.data.store.categories.filter(filterNonNullish).map((rootCategory) => {
           const children = rootCategory.children.filter(filterNonNullish)
-          const childrenLayouts = categoryChildrenLayouts(children)
+          const verticalChildrenLayouts = verticalCategoryChildrenLayouts(children)
           return (
             <Box
               key={rootCategory.id}
-              pad={'large'}
+              pad={{ horizontal: 'medium', vertical: 'large' }}
               direction="column"
               background={CATEGORY_NAME_COLOR_MAP[rootCategory.name] ?? 'border'}
             >
-              <Stack fill="horizontal">
-                <Box direction="column" gap="large">
-                  <Box id={`index-category-${rootCategory.id}`} justify="center">
-                    <Button
-                      href={route.category.detail(rootCategory)}
-                      raisePosition={true}
-                      testId={`index--category-nav--${rootCategory.id}`}
-                    >
-                      <Heading level={2} margin="none">
-                        {rootCategory.name}
-                      </Heading>
-                    </Button>
-                  </Box>
-                  <Box width="100%" justify="between">
-                    {children.map((childCategory, index) => (
-                      <Box
-                        key={childCategory.id}
-                        basis={childrenLayouts[index].basis}
-                        justify={childrenLayouts[index].justify}
-                      >
-                        <Box id={`index-category-${childCategory.id}`}>
-                          <Button
-                            href={route.category.detail(childCategory)}
-                            raisePosition={true}
-                            size="small"
-                            testId={`index--category-nav--${childCategory.id}`}
-                          >
-                            <Text as="h3">{childCategory.name}</Text>
-                          </Button>
+              <ResponsiveConsumer>
+                {(size): ReactNode => (
+                  <>
+                    {size === 'small' && (
+                      <Stack fill="horizontal">
+                        <Box direction={'row'} justify="between">
+                          <Box basis="1/2" justify="start" align="center">
+                            <Button
+                              id={`index-category-${rootCategory.id}`}
+                              href={route.category.detail(rootCategory)}
+                              raisePosition={true}
+                              testId={`index--category-nav--${rootCategory.id}`}
+                            >
+                              <Text as="h2">{rootCategory.name}</Text>
+                            </Button>
+                          </Box>
+                          <Box direction={'column'} gap={'medium'} align="start" basis="1/2">
+                            {children.map((childCategory) => (
+                              <Box key={childCategory.id}>
+                                <Box id={`index-category-${childCategory.id}`}>
+                                  <Button
+                                    href={route.category.detail(childCategory)}
+                                    raisePosition={true}
+                                    size="small"
+                                    testId={`index--category-nav--${childCategory.id}`}
+                                  >
+                                    <Text as="h3" size="xsmall">
+                                      {childCategory.name}
+                                    </Text>
+                                  </Button>
+                                </Box>
+                              </Box>
+                            ))}
+                          </Box>
                         </Box>
-                      </Box>
-                    ))}
-                  </Box>
-                </Box>
-                <Diagram
-                  connections={rootCategory.children.filter(filterNonNullish).map((child) => ({
-                    fromTarget: `index-category-${rootCategory.id}`,
-                    toTarget: `index-category-${child.id}`,
-                    thickness: 'xsmall',
-                    type: 'rectilinear',
-                    anchor: 'vertical',
-                    color: 'graph-0',
-                  }))}
-                />
-              </Stack>
+                        <Diagram
+                          connections={rootCategory.children.filter(filterNonNullish).map((child) => ({
+                            fromTarget: `index-category-${rootCategory.id}`,
+                            toTarget: `index-category-${child.id}`,
+                            thickness: 'xsmall',
+                            type: 'rectilinear',
+                            anchor: 'horizontal',
+                            color: 'graph-0',
+                          }))}
+                        />
+                      </Stack>
+                    )}
+                    {size !== 'small' && (
+                      <Stack fill="horizontal">
+                        <Box direction="column" gap="large">
+                          <Box id={`index-category-${rootCategory.id}`} justify="center">
+                            <Button
+                              href={route.category.detail(rootCategory)}
+                              raisePosition={true}
+                              testId={`index--category-nav--${rootCategory.id}`}
+                            >
+                              <Heading level={2} margin="none">
+                                {rootCategory.name}
+                              </Heading>
+                            </Button>
+                          </Box>
+                          <Box width="100%" justify="between">
+                            {children.map((childCategory, index) => (
+                              <Box
+                                key={childCategory.id}
+                                basis={verticalChildrenLayouts[index].basis}
+                                justify={verticalChildrenLayouts[index].justify}
+                              >
+                                <Box id={`index-category-${childCategory.id}`}>
+                                  <Button
+                                    href={route.category.detail(childCategory)}
+                                    raisePosition={true}
+                                    size="small"
+                                    testId={`index--category-nav--${childCategory.id}`}
+                                  >
+                                    <Text as="h3">{childCategory.name}</Text>
+                                  </Button>
+                                </Box>
+                              </Box>
+                            ))}
+                          </Box>
+                        </Box>
+                        <Diagram
+                          connections={rootCategory.children.filter(filterNonNullish).map((child) => ({
+                            fromTarget: `index-category-${rootCategory.id}`,
+                            toTarget: `index-category-${child.id}`,
+                            thickness: 'xsmall',
+                            type: 'rectilinear',
+                            anchor: 'vertical',
+                            color: 'graph-0',
+                          }))}
+                        />
+                      </Stack>
+                    )}
+                  </>
+                )}
+              </ResponsiveConsumer>
             </Box>
           )
         })}
